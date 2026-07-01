68c0286a073cabad12c8f1d5319677ea5080932f
uday <palepuday@gmail.com>
2026-07-01 10:28:07 +0530

    test2

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket        = "vsu-terraform-statefile"
-    key           = "dev/terraform.tfstate"
+    key           = "prod/terraform.tfstate"
     region        = "us-east-1"
     encrypt       = true
     use_lockfile  = true
