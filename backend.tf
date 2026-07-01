f1f3f44e714e33f068b509568e816a2e5286ecc7
uday2210 <96786677+uday2210@users.noreply.github.com>
2026-07-01 10:35:09 +0530

    Revert "ec2_s3_create"

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket        = "vsu-terraform-statefile"
-    key           = "prod/terraform.tfstate"
-    region        = "us-east-1"
-    encrypt       = true
-    use_lockfile  = true
-  }
-}
--- outputs.tf
+++
@@ -1,3 +0,0 @@
-output "instance_id" {
-  value = aws_instance.ec2.id
-}
